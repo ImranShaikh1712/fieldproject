1493bebbcd98637aff0f5a0fadde1ba92b23ee9f
Imran shaikh <imranvub45@gmail.com>
2025-03-16 11:41:12 +0530

    first-update

---
+++ header.js
@@ -0,0 +1,4 @@
+function toggleLogin() {
+    var loginOptions = document.getElementById("loginOptions");
+    loginOptions.style.display = (loginOptions.style.display === "block") ? "none" : "block";
+}
